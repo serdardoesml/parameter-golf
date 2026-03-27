ae4f75ef8494981b2a5ab47ca26156d4065150b0
Serdar Gülbahar <84625111+serdardoesml@users.noreply.github.com>
2026-03-27 20:42:28 +0100

    reduce layers

--- records/track_non_record_16mb/2026-03-27_UT/train_gpt.py
+++ records/track_non_record_16mb/2026-03-27_UT/train_gpt.py
@@ -61,7 +61,7 @@ class Hyperparameters:
 
     # Model shape.
     vocab_size = int(os.environ.get("VOCAB_SIZE", 1024))
-    num_layers = int(os.environ.get("NUM_LAYERS", 4))
+    num_layers = int(os.environ.get("NUM_LAYERS", 2))
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 4))
     model_dim = int(os.environ.get("MODEL_DIM", 768))
     num_heads = int(os.environ.get("NUM_HEADS", 16))
