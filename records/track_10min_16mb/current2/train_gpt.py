71aaed9742c8230efee0aaa4fbfb32317fa11485
Serdar Gülbahar <84625111+serdardoesml@users.noreply.github.com>
2026-04-29 20:42:04 +0200

    4.375

--- records/track_10min_16mb/current2/train_gpt.py
+++ records/track_10min_16mb/current2/train_gpt.py
@@ -247,7 +247,7 @@ class Hyperparameters:
     model_dim = int(os.environ.get("MODEL_DIM", 512))
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 4))
     num_heads = int(os.environ.get("NUM_HEADS", 8))
-    mlp_mult = float(os.environ.get("MLP_MULT", 4.4))
+    mlp_mult = float(os.environ.get("MLP_MULT", 4.375))
     skip_gates_enabled = bool(int(os.environ.get("SKIP_GATES_ENABLED", "1")))
     tie_embeddings = bool(int(os.environ.get("TIE_EMBEDDINGS", "1")))
     logit_softcap = float(os.environ.get("LOGIT_SOFTCAP", 3e1))
